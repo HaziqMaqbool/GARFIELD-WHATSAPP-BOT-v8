6b3df10b82d1b4034834169ea7edd3a36f2053b6
HaziqMaqbool <103175881+HaziqMaqbool@users.noreply.github.com>
2022-07-11 20:22:09 +0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -16,25 +16,26 @@ global.APIKeys = {
 global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
 global.emoji02 = ['🦋'] //👈 Enter your Emoji
 global.notice = ` *Hi 🥰 I m*
-*ＮＯＩＺ Ξ*
-*I am 16 years old developer...*
-*From Sri Lanka 🇱🇰*
+*H A Z I Q*
+*I am 18  years old developer...*
+*From Pakistan 🇵🇰*
 *I am a Learning.*
 *Not more..*
 *Also I am Student*
-*Follow My Facebook - https://www.facebook.com/zenoixnoize*`  // 👈 Enter Your details and Social Media Links and Anything
-global.owner =  ['+94711502119'] //👈  Enter Your number
-global.premium =  ['+94711502119'] //👈  Enter Your number
-global.ownernomer = '+94711502119' //👈  Enter Your number
-global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = 'ＧＡＲＦＩＥＬＤ ＢＯＴ ｖ8.3' //👈 Enter Your Bot name
-global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖦𝖺𝗋𝖿𝗂𝖾𝗅𝖽 𝖡𝗈𝗍' //👈 Enter Your Name
-global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
-global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
+ // 👈 Enter Your details and Social Media Links and Anything
+global.owner =  ['+7 950 012-47-90'] //👈  Enter Your number
+global.premium =  ['+7 950 012-47-90'] //👈  Enter Your number
+global.ownernomer = '+7 950 012-47-90' //👈  Enter Your number
+global.ownername = 'H A Z I Q' //👈 Enter Your name
+global.botname = 'H A Z I X ＢＯＴ ｖ8.3' //👈 Enter Your Bot name
+global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 H A Z I Q 𝖡𝗈𝗍' //👈 Enter Your Name
+
+global.region = 'Pakistan,Punjab,Kasur ' // 👈 You Can change this your choice
 global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
 global.myweb = 'https://www.facebook.com/garfieldbots/' // 👈 Enter your Social media link to follow now button
-global.packname = 'GARFIELD-BOTv8'  // 👈 You Can change this your choice 
-global.author = 'NOIZE DEVELOPMENT' // 👈 You Can change this your choice 
+global.packname = '❤H A Z I Q❤ (+7 950 012-47-90)
+// 👈 You Can change this your choice 
+global.author = 'HAZIQ DEVELOPMENT' // 👈 You Can change this your choice 
 global.sessionName = 'session' // 👈 You Can change this your choice
 global.prefa = ['','!','.','🛰️','⚓','🍎'] // 👈 You Can change this your choice
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
